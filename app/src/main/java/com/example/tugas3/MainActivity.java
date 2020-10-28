8d3f3b1d3250487a04f03d0b96681bbc21563b17
ardicandra007@gmail.com <ardicandra007@gmail.com>
2020-10-28 21:00:46 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas3/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tugas3;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePicker(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.timepicker));
+    }
+
+}
